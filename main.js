529ffbd43e4d426c3dbdf93b232c60790dbb53c0
Filmoda <adnanaittaher2@gmail.com>
2026-05-11 03:08:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -164,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
             filters: ['All', 'UGC Ads', 'Product Ads', 'SaaS', 'Corporate Brand Videos', 'Trailers'],
             items: [
                 {
-                    title: 'Nike Ad2',
+                    title: 'Nike Ad',
                     tag: 'Product Ads',
                     bg: 'linear-gradient(45deg, #2b5876, #4e4376)',
                     poster: 'IMG/Media/NikeFastAdVoca.png',
